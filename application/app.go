b0622f2a3a3c1a97c8bb2b05c44cd5e8344ce9dd
arslanoktay <oktay16arslan@hotmail.com>
2024-07-29 03:18:49 +0300

    context notes

--- application/app.go
+++ application/app.go
@@ -63,8 +63,13 @@ func (a *App) Start(ctx context.Context) error {
 
 		return server.Shutdown(timeout)
 	}
-
-	return nil
 }
 
 // Graceful Shutdown -> Using for redis, when a get signal shutdown db. Signtint with context library
+
+// Context -> Using context to share values between scopes, deadlines or timeouts.
+// For sharing value, funcs take same context as parameter, we can give value using context.WithValue(context,key,value). context.Value(key) will give the value
+// Second is cancelation context.withcancel(context) will return context and cancel. if cancel triggers, context.done will be happen. We use select for that matters.
+// timeouts are similiar, witchcancel(context, timeouts time) when time ups and context is still working, context.Done() will trigger
+// context creating types  WithCancel, WithDeadline, WithTimeout, or WithValue
+// When cancel happens stops all children contexts aswell
